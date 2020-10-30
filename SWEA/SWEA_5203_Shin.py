bdf0d9d0f4f2105e86fbfcc57a8e534c5a4627bd
sakwook-real <sakwook@gmail.com>
2020-10-30 13:20:27 +0900

    SWEA 1030

--- SWEA/SWEA_5203_Shin.py
+++ SWEA/SWEA_5203_Shin.py
@@ -1,26 +1,59 @@
 # SWEA 5203 베이비진 게임
 # SWEA 5203
 
-# Created by sw0817 on 2020. 08. 07..
+# Created by sw0817 on 2020. 10. 30..
 # Copyright © 2020 sw0817. All rights reserved.
 
 # See : https://swexpertacademy.com/main/learn/course/lectureProblemViewer.do
 
 
-# 원소수가 3개인 부분집합을 생성
-# 1이 3개인 6자리 2진수 --> 0도 3개
-cards = [5,4,1,2,3,6]
+def baby_gin():
+    global result
+    for i in range(len(card_set)):
+        if i % 2 == 0:
+            player1[card_set[i]] += 1
+            if player1[card_set[i]] == 3:
+                result = 1
+                return
+            else:
+                check = 0
+                for j in range(card_set[i] - 2, card_set[i] + 3):
+                    if j < 0 or 10 <= j:
+                        continue
+                    else:
+                        if player1[j] > 0:
+                            check += 1
+                            if check == 3:
+                                result = 1
+                                return
+                        else:
+                            check = 0
 
-for subset in range(1 << 6):
-#cards.sort()
-    A, B = [], []
-    for i in range(6):
-        if subset & (1 << i):
-            A.append(cards[i])
         else:
-            B.append(cards[i])
+            player2[card_set[i]] += 1
+            if player2[card_set[i]] == 3:
+                result = 2
+                return
+            else:
+                check = 0
+                for j in range(card_set[i] - 2, card_set[i] + 3):
+                    if j < 0 or 10 <= j:
+                        continue
+                    else:
+                        if player2[j] > 0:
+                            check += 1
+                            if check == 3:
+                                result = 2
+                                return
+                        else:
+                            check = 0
 
-    if len(A) == len(B):
-        print(A, B)
 
-# 부분집합과 조합은 관계가 있구나.
+T = int(input())
+for tc in range(1, T+1):
+    card_set = list(map(int, input().split()))
+    player1 = [0] * 10
+    player2 = [0] * 10
+    result = 0
+    baby_gin()
+    print('#{} {}'.format(tc, result))
