a31fd734fd0fd8acac94596517a2b8531543ffbf
sakwook-real <sakwook@gmail.com>
2020-12-17 20:55:59 +0900

    Beakjoon 1217

---
+++ Baekjoon problems/Baekjoon_1846_Shin.py
@@ -0,0 +1,12 @@
+# 백준 1402 아무래도이문제는A번난이도인것같다
+# Baekjoon 1402
+
+# Created by sw0817 on 2020. 12. 17..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/1402
+
+T = int(input())
+for _ in range(T):
+    input()
+    print('yes')
