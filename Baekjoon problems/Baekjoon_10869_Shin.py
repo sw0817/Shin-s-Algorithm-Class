97cb350af20ac04c71aad492930b819484e5f2b0
sakwook-real <sakwook@gmail.com>
2021-01-06 22:36:32 +0900

    Baekjoon 0106 #7

---
+++ Baekjoon problems/Baekjoon_10869_Shin.py
@@ -0,0 +1,15 @@
+# 백준 10869 사칙연산
+# Baekjoon 10869
+
+# Created by sw0817 on 2020. 01. 06..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/10869
+
+A, B = map(int, input().split())
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
