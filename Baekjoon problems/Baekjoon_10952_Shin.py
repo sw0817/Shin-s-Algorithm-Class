a54181fa275ba1a107ffccd63ccc9e38cef18418
sakwook-real <sakwook@gmail.com>
2021-01-06 22:42:11 +0900

    Baekjoon 0106 #9

---
+++ Baekjoon problems/Baekjoon_10952_Shin.py
@@ -0,0 +1,13 @@
+# 백준 10952 A+B - 5
+# Baekjoon 10952
+
+# Created by sw0817 on 2020. 01. 06..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/10952
+
+while True:
+    A, B = map(int, input().split())
+    if A == B == 0:
+        break
+    print(A+B)
