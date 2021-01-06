4497caed42ec3226dfc3f8657119caa69496830f
sakwook-real <sakwook@gmail.com>
2021-01-06 22:46:25 +0900

    Baekjoon 0106 #11

---
+++ Baekjoon problems/Baekjoon_11720_Shin.py
@@ -0,0 +1,16 @@
+# 백준 11720 숫자의 합
+# Baekjoon 11720
+
+# Created by sw0817 on 2020. 01. 06..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/11720
+
+N = int(input())
+number = input()
+
+result = 0
+for i in range(N):
+    result += int(number[i])
+
+print(result)
