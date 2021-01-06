7c68a931ceab79f31f7a7b0a058c0cf8da5d18b8
sakwook-real <sakwook@gmail.com>
2021-01-06 22:22:43 +0900

    Baekjoon 0106 #3

---
+++ Baekjoon problems/Baekjoon_2739_Shin.py
@@ -0,0 +1,12 @@
+# 백준 2739 구구단
+# Baekjoon 2739
+
+# Created by sw0817 on 2020. 01. 06..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2739
+
+N = int(input())
+
+for i in range(1, 10):
+    print('{} * {} = {}'.format(N, i, N*i))
