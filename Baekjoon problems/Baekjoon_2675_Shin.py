17f4a7a1edc17e66c915d47b4be5498d50ee57c2
sakwook-real <sakwook@gmail.com>
2021-01-06 22:20:26 +0900

    Baekjoon 0106 #2

---
+++ Baekjoon problems/Baekjoon_2675_Shin.py
@@ -0,0 +1,18 @@
+# 백준 2675 문자열 반복
+# Baekjoon 2675
+
+# Created by sw0817 on 2020. 01. 06..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2675
+
+T = int(input())
+
+for _ in range(T):
+    N, S = map(str, input().split())
+    N = int(N)
+    for i in range(len(S)):
+        for _ in range(N):
+            print(S[i], end='')
+
+    print()
