7824af0b8618a224d35236fd95e7f849be3167dc
sakwook-real <sakwook@gmail.com>
2021-02-19 22:43:06 +0900

    통나무 옮기기 준비

--- Baekjoon problems/Baekjoon_1938_Shin.py
+++ Baekjoon problems/Baekjoon_1938_Shin.py
@@ -1,7 +1,7 @@
-# 백준 1938 게임 개발
+# 백준 1938 통나무 옮기기
 # Baekjoon 1938
 
-# Created by sw0817 on 2020. 12. 21..
+# Created by sw0817 on 2021. 02. 19..
 # Copyright © 2020 sw0817. All rights reserved.
 
 # See : https://www.acmicpc.net/problem/1938
