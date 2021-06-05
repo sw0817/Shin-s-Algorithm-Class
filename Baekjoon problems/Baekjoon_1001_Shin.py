5e4fc03cabcf8d7f26fd2791f15538352b7ae0e5
sw0817 <sakwook@gmail.com>
2021-06-05 23:56:30 +0900

    Baekjoon 0605 #1

---
+++ Baekjoon problems/Baekjoon_1001_Shin.py
@@ -0,0 +1,10 @@
+# 백준 1001 A-B
+# Baekjoon 1001
+
+# Created by sw0817 on 2021. 06. 05..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/1001
+
+A, B = map(int, input().split())
+print(A-B)
