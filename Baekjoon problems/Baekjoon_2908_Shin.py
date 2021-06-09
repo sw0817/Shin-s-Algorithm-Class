fc835667bd07bdfdb25f13a14dd3b0102dbeb7f5
sakwook-real <sakwook@gmail.com>
2021-06-09 19:55:51 +0900

    Baekjoon 0609 #1

---
+++ Baekjoon problems/Baekjoon_2908_Shin.py
@@ -0,0 +1,19 @@
+# 백준 2908 상수
+# Baekjoon 2908
+
+# Created by sw0817 on 2021. 06. 09..
+# Copyright © 2020 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2908
+
+A, B = map(str, input().split())
+a, b = "", ""
+for i in range(3):
+    a += A[2-i]
+    b += B[2-i]
+
+a, b = int(a), int(b)
+if a > b:
+    print(a)
+else:
+    print(b)
