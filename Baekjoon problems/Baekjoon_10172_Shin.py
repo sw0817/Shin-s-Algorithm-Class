544ddb38fb10f40f5563e5e7b0ace41d86eec993
sakwook-real <sakwook@gmail.com>
2021-06-14 23:52:06 +0900

    Baekjoon 0614 #1

---
+++ Baekjoon problems/Baekjoon_10172_Shin.py
@@ -0,0 +1,23 @@
+# 백준 10172 개
+# Baekjoon 10172
+
+# Created by sw0817 on 2021. 06. 14..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/10172
+
+'''
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+'''
+
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
+
+# 역슬래시를 출력하려면 두 번 역슬래시를 입력하면 된다.
