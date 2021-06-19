895677982ab9f8823e365f835c82da0311bf885a
sakwook-real <sakwook@gmail.com>
2021-06-19 23:09:45 +0900

    Baekjoon 0619 #1

---
+++ Baekjoon problems/Baekjoon_10998_Shin.py
@@ -0,0 +1,10 @@
+# 백준 10998 AxB
+# Baekjoon 10998
+
+# Created by sw0817 on 2021. 06. 19..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/10998
+
+A, B = map(int, input().split())
+print(A * B)
