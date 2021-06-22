e8f29e650b646af704678e357e12d831c584ed60
sakwook-real <sakwook@gmail.com>
2021-06-22 23:04:23 +0900

    Baekjoon 0622 #1

---
+++ Baekjoon problems/Baekjoon_10171_Shin.py
@@ -0,0 +1,12 @@
+# 백준 10171 고양이
+# Baekjoon 10171
+
+# Created by sw0817 on 2021. 06. 22..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/10171
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
