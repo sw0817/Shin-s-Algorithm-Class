3ad6a338cef8d67aaa9a7a0c6947be05f2e34f32
sakwook-real <sakwook@gmail.com>
2021-07-08 22:00:46 +0900

    Baekjoon 0708 #2

--- Baekjoon problems/Baekjoon_1430_Shin.py
+++ Baekjoon problems/Baekjoon_1430_Shin.py
@@ -10,6 +10,41 @@ from collections import deque
 
 N, R, D, X, Y = map(int, input().split())
 
+adj = [[] for _ in range(N+1)]
+tops = deque()
+tops.append((X, Y))
+for i in range(1, N+1):
+    x, y = map(int, input().split())
+    tops.append((x, y))
+    for j in range(i):
+        c, r = tops[j]
+        if (abs(c-x) ** 2 + abs(r-y) ** 2) ** 0.5 <= R:
+            adj[i].append(j)
+            adj[j].append(i)
+
+visited = [0] * (N+1)
+visited[0] = 1
+result = 0
+mag = 1
+queue = deque()
+for n in adj[0]:
+    queue.append(n)
+    visited[n] = 1
+
+while queue:
+    for _ in range(len(queue)):
+        n = queue.popleft()
+        result += mag * D
+        for m in adj[n]:
+            if not visited[m]:
+                visited[m] = 1
+                queue.append(m)
+    mag /= 2
+
+print(round(result, 3))
+
+####################
+
 # tops = deque()
 # for _ in range(N):
 #     x, y = map(int, input().split())
@@ -43,37 +78,41 @@ N, R, D, X, Y = map(int, input().split())
 
 ###########
 
-move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+# move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+#
+# arr = [[0] * 1001 for _ in range(1001)]
+# for _ in range(N):
+#     x, y = map(int, input().split())
+#     arr[x][y] = 1
+#
+# result = 0
+# mag = 1
+# queue = deque()
+# queue.append((X, Y))
+# while queue:
+#     for _ in range(len(queue)):
+#         visited = [[0] * 1001 for _ in range(1001)]
+#         x, y = queue.popleft()
+#         visited[x][y] = 1
+#         queue2 = deque()
+#         queue2.append((x, y))
+#         while queue2:
+#             x2, y2 = queue2.popleft()
+#             for dx, dy in move:
+#                 nx, ny = x2 + dx, y2 + dy
+#                 if nx < 0 or 1000 < nx or ny < 0 or 1000 < ny:
+#                     continue
+#                 if (abs(nx-x) ** 2 + abs(ny-y) ** 2) ** 0.5 <= R and not visited[nx][ny]:
+#                     if arr[nx][ny] == 1:
+#                         arr[nx][ny] = 2
+#                         result += D * mag
+#                         queue.append((nx, ny))
+#                     queue2.append((nx, ny))
+#                     visited[nx][ny] = 1
+#     mag /= 2
+#
+# print(round(result, 3))
 
-arr = [[0] * 1001 for _ in range(1001)]
-for _ in range(N):
-    x, y = map(int, input().split())
-    arr[x][y] = 1
+###############
 
-result = 0
-mag = 1
-queue = deque()
-queue.append((X, Y))
-while queue:
-    for _ in range(len(queue)):
-        visited = [[0] * 1001 for _ in range(1001)]
-        x, y = queue.popleft()
-        visited[x][y] = 1
-        queue2 = deque()
-        queue2.append((x, y))
-        while queue2:
-            x2, y2 = queue2.popleft()
-            for dx, dy in move:
-                nx, ny = x2 + dx, y2 + dy
-                if nx < 0 or 1000 < nx or ny < 0 or 1000 < ny:
-                    continue
-                if (abs(nx-x) ** 2 + abs(ny-y) ** 2) ** 0.5 <= R and not visited[nx][ny]:
-                    if arr[nx][ny] == 1:
-                        arr[nx][ny] = 2
-                        result += D * mag
-                        queue.append((nx, ny))
-                    queue2.append((nx, ny))
-                    visited[nx][ny] = 1
-    mag /= 2
 
-print(round(result, 3))
