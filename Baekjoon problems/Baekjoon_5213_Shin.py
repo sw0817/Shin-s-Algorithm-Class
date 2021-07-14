c745d301f1151d758f99b08bc2429367bec02855
sakwook-real <sakwook@gmail.com>
2021-07-15 00:03:59 +0900

    Baekjoon 0715 #1

--- Baekjoon problems/Baekjoon_5213_Shin.py
+++ Baekjoon problems/Baekjoon_5213_Shin.py
@@ -1,7 +1,7 @@
 # 백준 5213 과외맨
 # Baekjoon 5213
 
-# Created by sw0817 on 2021. 07. 11..
+# Created by sw0817 on 2021. 07. 15..
 # Copyright © 2021 sw0817. All rights reserved.
 
 # See : https://www.acmicpc.net/problem/5213
