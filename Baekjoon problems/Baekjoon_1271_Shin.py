6ea76e810a5618e4ee1bef6f8e4ee1049b97c739
sakwook-real <sakwook@gmail.com>
2021-07-20 23:48:24 +0900

    Baekjoon 0720 #1

---
+++ Baekjoon problems/Baekjoon_1271_Shin.py
@@ -0,0 +1,11 @@
+# 백준 1271 엄청난 부자2
+# Baekjoon 1271
+
+# Created by sw0817 on 2021. 07. 20..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/1271
+
+n, m = map(int, input().split())
+print(n // m)
+print(n % m)
