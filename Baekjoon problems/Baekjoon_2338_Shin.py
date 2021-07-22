1168d670d2eb40af84135c56c7817cde0b1712ba
sakwook-real <sakwook@gmail.com>
2021-07-22 21:12:26 +0900

    Baekjoon 0722 #1

---
+++ Baekjoon problems/Baekjoon_2338_Shin.py
@@ -0,0 +1,13 @@
+# 백준 2338 긴자리 계산
+# Baekjoon 2338
+
+# Created by sw0817 on 2021. 07. 22..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2338
+
+A = int(input())
+B = int(input())
+print(A+B)
+print(A-B)
+print(A*B)
