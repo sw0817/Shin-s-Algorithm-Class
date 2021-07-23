2e6254309e32bef5878a6878e3030d0acf368791
sakwook-real <sakwook@gmail.com>
2021-07-23 21:14:57 +0900

    Baekjoon 0723 #3

--- Baekjoon problems/Baekjoon_2263_Shin.py
+++ Baekjoon problems/Baekjoon_2263_Shin.py
@@ -6,17 +6,34 @@
 
 # See : https://www.acmicpc.net/problem/2263
 
+'''
+15
+8 7 10 2 15 5 12 3 9 4 14 1 6 11 13
+8 10 7 15 12 5 2 9 14 4 6 13 11 1 3
+
+13
+8 7 10 2 12 5 3 9 4 1 6 11 13
+8 10 7 12 5 2 9 4 6 13 11 1 3
+'''
+
+
 from sys import setrecursionlimit
 
 def preOrder(inLeft, inRight, postLeft, postRight):
+    # print()
     # print(inOrder[inLeft], inOrder[inRight], postOrder[postLeft], postOrder[postRight])
     if postLeft <= postRight and inLeft <= inRight:
+        print()
+        print(inOrder[inLeft], inOrder[inRight], postOrder[postLeft], postOrder[postRight])
+        # print("hit")
         parent = postOrder[postRight]
-        print(parent, end=" ")
+        # print(parent, end=" ")
         pIdx = inPos[parent]
+        # print(parent, pIdx)
         preOrder(inLeft, pIdx-1, postLeft, postLeft+pIdx-inLeft-1) # 부모 노드 기준 왼쪽
         preOrder(pIdx+1, inRight, postRight+pIdx-inRight, postRight-1)
-
+    # else:
+    #     print(inLeft, inRight, postLeft, postRight)
 
 setrecursionlimit(10 ** 6)
 
@@ -26,5 +43,5 @@ postOrder = list(map(int, input().split()))
 inPos = [0] * (N+1)
 for idx, i in enumerate(inOrder):
     inPos[i] = idx
-# print(inPos)
+print(inPos)
 preOrder(0, N-1, 0, N-1)
