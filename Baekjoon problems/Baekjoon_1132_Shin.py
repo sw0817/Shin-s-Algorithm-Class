ab6255da1a016cf500a795cf6a98a4d7a7e97193
sakwook-real <sakwook@gmail.com>
2021-07-27 22:51:08 +0900

    Baekjoon 0727 #2

--- Baekjoon problems/Baekjoon_1132_Shin.py
+++ Baekjoon problems/Baekjoon_1132_Shin.py
@@ -8,21 +8,37 @@
 
 N = int(input())
 alpha = [[0] for _ in range(10)]
-exist = [0] * 10
+n = [1] * 10
+nums = []
+nonZero = set()
 for i in range(10):
     alpha[i].append(chr(65+i))
-first = set()
 
 for _ in range(N):
     num = input()
-    first.add(num[0])
-    exist[ord(num[0])] = 1
+    nums.append(num)
+    nonZero.add(num[0])
     l = len(num)
     for i in range(l):
         alp = ord(num[l-i-1])-65
         alpha[alp][0] += 10 ** i
 
-alpha.sort(reverse=True)
+alpha.sort()
 
-# 중간 점검
+for i in range(10):
+    if not alpha[i][1] in nonZero:
+        alpha[i][1] = 0
+        break
 
+cnt = 1
+for i in range(10):
+    if alpha[i][1]:
+        alpha[i][1] = cnt
+        cnt += 1
+
+result = 0
+
+for info in alpha:
+    result += info[0] * info[1]
+
+print(result)
