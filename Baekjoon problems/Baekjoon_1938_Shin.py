d71768394c9e4401c8ac5ab387bbd4abd1e79304
sakwook-real <sakwook@gmail.com>
2021-08-02 21:46:17 +0900

    Baekjoon 0802 #1

--- Baekjoon problems/Baekjoon_1938_Shin.py
+++ Baekjoon problems/Baekjoon_1938_Shin.py
@@ -6,136 +6,79 @@
 
 # See : https://www.acmicpc.net/problem/1938
 
-# 상태
-# 통나무 가로 = 2 세로 = 3
-# 최종 가로 = 4 세로 = 5
+from collections import deque
 
-next = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+def rotate(R, C):
+    for i in range(R-1, R+2):
+        for j in range(C-1, C+2):
+            if arr[i][j] == '1':
+                return False
+    return True
 
 
-def move(r, c, cnt, type):
-    global result
-
-    for visit in visited:
-        print(visit)
-    print()
-    for visit2 in visited2:
-        print(visit2)
-    print()
-
-    if array[r][c] == '4' and type == 2:
-        if cnt < result:
-            result = cnt
-            return
-
-    elif array[r][c] == '5' and type == 3:
-        if cnt < result:
-            result = cnt
-            return
-
-    if type == 2:
-        visited[r][c] = cnt
-        if r != 0 and r != N-1:
-            a = 1
-            if array[r-1][c] == 1 or array[r-1][c-1] == 1 or array[r-1][c+1] == 1 or array[r+1][c] == 1 or array[r+1][c-1] == 1 or array[r+1][c+1] == 1:
-                a = 0
-            if a:
-                if visited2[r][c] != -1 and visited2[r][c] > cnt+1 or visited2[r][c] == -1:
-                    move(r, c, cnt+1, 3)
-
-    elif type == 3:
-        visited2[r][c] = cnt
-
-        if c != 0 and c != N-1:
-            a = 1
-            if array[r-1][c+1] == 1 or array[r+1][c+1] == 1 or array[r][c+1] == 1 or array[r+1][c-1] == 1 or array[r-1][c-1] == 1 or array[r][c-1] == 1:
-                a = 0
-            if a:
-
-                if visited[r][c] != -1 and visited[r][c] > cnt+1 or visited[r][c] == -1:
-                    move(r, c, cnt+1, 2)
-
-    for i in range(4):
-        nr = r + next[i][0]
-        nc = c + next[i][1]
-
-        if type == 2:
-            if nr < 0 or nc < 1 or N <= nr or N-1 <= nc or visited[nr][nc] != -1 and visited[nr][nc] <= cnt+1:
-                continue
-
-            if i == 0 or i == 2:
-                if array[nr][nc] == 1 or array[nr][nc-1] == 1 or array[nr][nc+1] == 1:
-                    continue
-
-            elif i == 1:
-                if array[nr][nc+1] == 1:
-                    continue
-
+def bfs():
+    cnt = -1
+    while queue:
+        cnt += 1
+        for _ in range(len(queue)):
+            r, c, v = queue.popleft()
+            if r == E[0] and c == E[1] and v == E[2]:
+                return cnt
+            if v == 0:
+                for dr, dc in move:
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < N and 1 <= nc < N - 1 and arr[nr][nc - 1] != '1' and arr[nr][nc] != '1' and arr[nr][nc + 1] != '1':
+                        if not visited[nr][nc][v]:
+                            visited[nr][nc][v] = 1
+                            queue.append((nr, nc, v))
             else:
-                if array[nr][nc-1] == 1:
-                    continue
+                for dr, dc in move:
+                    nr, nc = dr + r, dc + c
+                    if nr < 1 or N - 1 <= nr or nc < 0 or N <= nc or arr[nr - 1][nc] == '1' or arr[nr][nc] == '1' or arr[nr + 1][nc] == '1':
+                        continue
+                    if not visited[nr][nc][v]:
+                        visited[nr][nc][v] = 1
+                        queue.append((nr, nc, v))
 
-            move(nr, nc, cnt+1, 2)
+            if 1 <= r < N - 1 and 1 <= c < N - 1 and rotate(r, c):
+                v = 1 if v == 0 else 0
+                if not visited[r][c][v]:
+                    visited[r][c][v] = 1
+                    queue.append((r, c, v))
+    return 0
 
 
-        elif type == 3:
-            if nr < 1 or nc < 0 or N-1 <= nr or N <= nc or visited2[nr][nc] != -1 and visited2[nr][nc] <= cnt+1:
-                continue
-
-            if i == 1 or i == 3:
-                if array[nr][nc] == 1 or array[nr+1][nc] == 1 or array[nr-1][nc] == 1:
-                    continue
-
-            elif i == 0:
-                if array[nr+1][nc] == 1:
-                    continue
-
-            else:
-                if array[nr-1][nc] == 1:
-                    continue
-
-            move(nr, nc, cnt+1, 3)
-
+move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 N = int(input())
-
-array = []
-B = 0
-E = 0
-start = [0, 0]
+arr = []
+start = []
+end = []
 for i in range(N):
     row = list(input())
     for j in range(N):
         if row[j] == 'B':
-            B += 1
-            if B == 2:
-                if j < N-1 and row[j+1] == 'B':
-                    row[j] = '2'
-                else:
-                    row[j] = '3'
-                start = [i, j]
-            else:
-                row[j] = '0'
+            start.append((i, j))
+        elif row[j] == 'E':
+            end.append((i, j))
+    arr.append(row)
 
-        if row[j] == 'E':
-            E += 1
-            if E == 2:
-                if j < N-1 and row[j+1] == 'E':
-                    row[j] = '4'
-                else:
-                    row[j] = '5'
-            else:
-                row[j] = '0'
-    array.append(row)
+visited = [[[0] * 2 for _ in range(N)] for _ in range(N)]
 
-result = 99999999999
+queue = deque()
 
-# 가로
-visited = [[-1] * N for _ in range(N)]
-# 세로
-visited2 = [[-1] * N for _ in range(N)]
+sr, sc = start[1]
+if start[0][0] == start[1][0]:
+    visited[sr][sc][0] = 0
+    queue.append((sr, sc, 0))
+else:
+    visited[sr][sc][1] = 0
+    queue.append((sr, sc, 1))
 
-move(start[0], start[1], 0, int(array[start[0]][start[1]]))
-# print(array[start[0]][start[1]])
+er, ec = end[1]
+if end[0][0] == end[1][0]:
+    E = (er, ec, 0)
+else:
+    E = (er, ec, 1)
 
-print(result)
+print(bfs())
