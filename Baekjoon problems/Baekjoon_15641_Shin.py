37a75c938a9c4ed9077f37dd4e348ca755fc5325
sakwook-real <sakwook@gmail.com>
2021-08-05 23:49:44 +0900

    Baekjoon 0805 #2

---
+++ Baekjoon problems/Baekjoon_15641_Shin.py
@@ -0,0 +1,20 @@
+# 백준 15641 SUPER SUPER BINARY SEARCH DELUXE 2.5: THE LEGEND OF THE GOLDEN MAZASSUMNIDA, EPISODE 2: THE MAZWAETL UNIVERSE, PART 2: THE PARALLEL UNIVERSE AND THE LOST MAZASSUMNIDA: GAME OF THE YEAR EDITION
+# Baekjoon 15641
+
+# Created by sw0817 on 2021. 08. 05..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/15641
+
+# 1~100 사이 자연수를 출력한다.
+# 단 하나의 자연수만 정답이며 정답은 계속 변화한다.
+# 정답보다 작은 수를 출력하면 33%에서 틀렸습니다를,
+# 큰 수를 출력하면 66%에서 틀렸습니다를 받는다.
+# 이분 탐색을 한다.
+# try 1. 50 => 66%
+# try 2. 25 => 66%
+# try 3. 12 => 66%
+# try 4. 6 => 66%
+# try 5. 3 => 33%
+# try 6. 5 => 66%
+# try 7. 4 => 맞았습니다!!
