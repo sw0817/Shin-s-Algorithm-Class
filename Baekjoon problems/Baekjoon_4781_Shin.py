1a230092e7bf3c47a3a913fd5721293a39b73b65
sakwook-real <sakwook@gmail.com>
2021-08-07 16:52:20 +0900

    Baekjoon 0807 #1

---
+++ Baekjoon problems/Baekjoon_4781_Shin.py
@@ -0,0 +1,10 @@
+# 백준 14918 더하기
+# Baekjoon 14918
+
+# Created by sw0817 on 2021. 08. 07..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/14918
+
+a, b = map(int, input().split())
+print(a+b)
