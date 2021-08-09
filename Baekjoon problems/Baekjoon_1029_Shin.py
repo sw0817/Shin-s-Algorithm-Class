b0616cec4c9080a7e3bd9c6a6a7c12a05b4bdb96
sakwook-real <sakwook@gmail.com>
2021-08-09 09:38:51 +0900

    Baekjoon 0809 #1

--- Baekjoon problems/Baekjoon_1029_Shin.py
+++ Baekjoon problems/Baekjoon_1029_Shin.py
@@ -1,34 +1,29 @@
 # 백준 1029 그림 교환
 # Baekjoon 1029
 
-# Created by sw0817 on 2021. 08. 08..
+# Created by sw0817 on 2021. 08. 09..
 # Copyright © 2021 sw0817. All rights reserved.
 
 # See : https://www.acmicpc.net/problem/1029
 
-from sys import stdin
-
-# 상태, 번호, 비용
-def dfs(num, state, cost):
-    result = dp[num][state][cost]
-
-    if state == 1 << N - 1:
-        return result
-
-    if result:
-        return result
+# 상태, 번호, 비용, 수
+def dfs(num, state, cost, cnt):
+    global result
+    if cost < dp[num][state]:
+        dp[num][state] = cost
+        result = max(result, cnt)
+    else:
+        return
 
     for i in range(1, N):
         temp = state | (1 << i)
         # i번을 이미 방문했거나, 현재 번호에서 i로 갈때 비용이 더 작거나
         if temp != state and cost <= arr[num][i]:
             # |는 하나라도 켜져 있으면 비트마스킹 켜주기
-            result = max(result, 1 + dfs(i, temp, arr[num][i]))
-
-    return result
+            dfs(i, temp, arr[num][i], cnt + 1)
 
 
-N = int(stdin.readline())
+N = int(input())
 
 arr = []
 for _ in range(N):
@@ -37,7 +32,7 @@ for _ in range(N):
     for char in info:
         arr[-1].append(int(char))
 # 상태, 번호, 비용
-dp = [[[0] * 10 for _ in range(1 << 15)] for _ in range(15)]
-print(1 + dfs(0, 1, 0))
-
-# 실패
+dp = [[10 for _ in range(1 << 15)] for _ in range(N)]
+result = 0
+dfs(0, 1, 0, 1)
+print(result)
