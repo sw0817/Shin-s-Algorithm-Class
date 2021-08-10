11034c26d3e57902805851c00746b5e386aaef4d
sakwook-real <sakwook@gmail.com>
2021-08-10 09:02:10 +0900

    Baekjoon 0810 #1

--- Baekjoon problems/Baekjoon_4781_Shin.py
+++ Baekjoon problems/Baekjoon_4781_Shin.py
@@ -1,10 +1,22 @@
-# 백준 14918 더하기
-# Baekjoon 14918
+# 백준 4781 사탕가게
+# Baekjoon 4781
 
 # Created by sw0817 on 2021. 08. 07..
 # Copyright © 2021 sw0817. All rights reserved.
 
-# See : https://www.acmicpc.net/problem/14918
+# See : https://www.acmicpc.net/problem/4781
 
-a, b = map(int, input().split())
-print(a+b)
+while True:
+    N, M = map(float, input().split())
+    N, M = int(N), int(M * 100 + 0.5)
+    dp = [0] * (M+1)
+    for i in range(1, N+1):
+        c, p = map(float, input().split())
+        c, p = int(c), int(p * 100 + 0.5)
+        for j in range(p, M+1):
+            dp[j] = max(dp[j], dp[j-p] + c)
+
+    if N == 0:
+        break
+
+    print(dp[M])
