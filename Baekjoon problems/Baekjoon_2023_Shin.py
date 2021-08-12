bb701f40cf21234a0dc4037589f1e9cc9b704964
sakwook-real <sakwook@gmail.com>
2021-08-12 09:12:27 +0900

    Baekjoon 0812 #1

--- Baekjoon problems/Baekjoon_2023_Shin.py
+++ Baekjoon problems/Baekjoon_2023_Shin.py
@@ -8,25 +8,52 @@
 
 # 8자리는 10,000,000
 
-n = 10000000
-a = [True] * n
-primes = set()
-for i in range(2, int(n ** 0.5) + 1):
-    if a[i]:
-        primes.add(i)
-        for j in range(2*i, n, i):
-            a[j] = False
-
-N = int(input())
-for prime in primes:
-    if 10 ** (N-1) <= prime < 10 ** N:
-        check = True
-        p = str(prime)
-        for j in range(1, len(p)-1):
-            if not int(p[:j]) in primes:
-                check = False
-                break
-        if check:
-            print(p)
+# n = 10000000
+# a = [True] * n
+# primes = set()
+# for i in range(2, int(n ** 0.5) + 1):
+#     if a[i]:
+#         primes.add(i)
+#         for j in range(2*i, n, i):
+#             a[j] = False
+#
+# N = int(input())
+# for prime in primes:
+#     if 10 ** (N-1) <= prime < 10 ** N:
+#         check = True
+#         p = str(prime)
+#         for j in range(1, len(p)-1):
+#             if not int(p[:j]) in primes:
+#                 check = False
+#                 break
+#         if check:
+#             print(p)
 
 # 메모리 초과
+
+def prime_check(num):
+    # print(num)
+    for i in range(2, int((int(num) ** 0.5)) + 1):
+        # 소수가 아님
+        if int(num) % i == 0:
+            return False
+
+    # 소수일 때
+    # N자리수가 되면 출력
+    if len(num) == N:
+        print(num)
+        return
+
+    # nums 숫자들을 더해준다
+    for i in nums:
+        prime_check(num + i)
+
+
+N = int(input())
+
+prime_num = ['2', '3', '5', '7']
+
+nums = ['1', '3', '7', '9']
+
+for i in prime_num:
+    prime_check(i)
