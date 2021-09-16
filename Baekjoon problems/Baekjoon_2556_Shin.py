e693179184bcaf9b1eebc3a5e0d6bb772ad005f2
sakwook-real <sakwook@gmail.com>
2021-09-16 11:13:25 +0900

    Baekjoon 0916 #1

---
+++ Baekjoon problems/Baekjoon_2556_Shin.py
@@ -0,0 +1,11 @@
+# 백준 2556 별 찍기 - 14
+# Baekjoon 2556
+
+# Created by sw0817 on 2021. 09. 16..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2556
+
+N = int(input())
+for _ in range(N):
+    print('*' * N)
