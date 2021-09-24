4511d16678adb168c8b5b5e0ddf64759660f7aa0
sakwook-real <sakwook@gmail.com>
2021-09-24 15:48:41 +0900

    Baekjoon 0924 #1

---
+++ Baekjoon problems/Baekjoon_1405_Shin.py
@@ -0,0 +1,50 @@
+# 백준 1405 미친 로봇
+# Baekjoon 1405
+
+# Created by sw0817 on 2021. 09. 24..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/1405
+
+from collections import deque
+
+def recur(i, j, n, p):
+    global result
+    if n == N:
+        return
+    for dr, dc, t_p in move:
+        nr, nc = i + dr, j + dc
+        if visited[nr][nc]:
+            result -= p * t_p / 100
+        else:
+            visited[nr][nc] = 1
+            recur(nr, nc, n+1, p * t_p / 100)
+            visited[nr][nc] = 0
+
+
+result = 1
+N, east, west, south, north = map(int, input().split())
+move = [(0, 1, east),(0, -1, west), (1, 0, south), (-1, 0, north)]
+queue = deque()
+visited = [[0] * (2 * N + 1) for _ in range(2 * N + 1)]
+visited[N][N] = 1
+for dr, dc, p in move:
+    nr, nc = N + dr, N + dc
+    visited[nr][nc] = 1
+    recur(nr, nc, 1, p / 100)
+    visited[nr][nc] = 0
+# queue.append((N, N, 1))
+# depth = 0
+# while queue and depth < N:
+#     depth += 1
+#     for _ in range(len(queue)):
+#         r, c, temp = queue.popleft()
+#         for dr, dc, n in move:
+#             nr, nc = r + dr, c + dc
+#             if visited[nr][nc]:
+#                 result -= temp * n / 100
+#             else:
+#                 visited[nr][nc] = 1
+#                 queue.append((nr, nc, temp * n / 100))
+
+print(result)
