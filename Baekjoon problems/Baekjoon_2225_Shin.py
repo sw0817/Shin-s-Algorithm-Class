c9c553b26ce2dd05aa5cae07d6dec2e37b350d93
sakwook-real <sakwook@gmail.com>
2021-09-26 22:16:52 +0900

    Baekjoon 0926 #1

---
+++ Baekjoon problems/Baekjoon_2225_Shin.py
@@ -0,0 +1,18 @@
+# 백준 2225 합분해
+# Baekjoon 2225
+
+# Created by sw0817 on 2021. 09. 26..
+# Copyright © 2021 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2225
+
+N, K = map(int, input().split())
+
+memo = [0] * (N + K + 1)
+
+n, memo[1] = 1, 1
+for i in range(2, N+K+1):
+    n *= i
+    memo[i] = n
+
+print(memo[N+K-1] // (memo[K-1] * memo[N]) % 1000000000)
