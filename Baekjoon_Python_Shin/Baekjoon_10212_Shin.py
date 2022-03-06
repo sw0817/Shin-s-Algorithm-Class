124684f6dedebe48c5d156abb09163f8d6eaef05
sakwook-real <sakwook@gmail.com>
2022-03-06 22:03:28 +0900

    Baekjoon 0306 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_10212_Shin.py
@@ -0,0 +1,17 @@
+# 백준 10423 Mystery
+# Baekjoon 10423
+
+# Created by sw0817 on 2022. 03. 06..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/10423
+
+import random
+
+N = random.randint(0, 1)
+if N:
+    print("Yonsei")
+else:
+    print("Korea")
+
+# 8번만의 성공. 12.5%의 운.
