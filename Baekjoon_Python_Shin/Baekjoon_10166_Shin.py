7ce5ea5f25f7b3ad95876b349eafc6acce8fb20a
sakwook-real <sakwook@gmail.com>
2022-04-27 20:40:32 +0900

    Baekjoon 0427 #1

--- Baekjoon_Python_Shin/Baekjoon_10166_Shin.py
+++ Baekjoon_Python_Shin/Baekjoon_10166_Shin.py
@@ -6,12 +6,17 @@
 
 # See : https://www.acmicpc.net/problem/10166
 
+from math import gcd
+
 D1, D2 = map(int, input().split())
-dir = set()
-dir.add(0)
+visited = [[0] * 2001 for _ in range(2001)]
+result = 0
 
 for i in range(D1, D2+1):
-    for j in range(1, i):
-        dir.add(j/i)
+    for j in range(1, i+1):
+        g = gcd(i, j)
+        if not visited[i//g][j//g]:
+            visited[i//g][j//g] = 1
+            result += 1
 
-print(len(dir))
+print(result)
