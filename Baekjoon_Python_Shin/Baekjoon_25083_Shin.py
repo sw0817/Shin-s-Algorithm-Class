ff387f78effef7dedaf8210de53a00aa2ea0f331
sakwook-real <sakwook@gmail.com>
2022-06-27 21:57:57 +0900

    Baekjoon 0627 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_25083_Shin.py
@@ -0,0 +1,9 @@
+# 백준 25083 새싹
+# Baekjoon 25083
+
+# Created by sw0817 on 2022. 06. 27..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/25083
+
+print("         ,r'" + '"' + "7\nr`-_   ,'  ,/\n \. " + '"' + ". L_r'\n   `~\/\n      |\n      |")
