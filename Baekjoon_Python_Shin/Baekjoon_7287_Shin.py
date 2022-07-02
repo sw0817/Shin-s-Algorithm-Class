679d705373e4c74c1eb59a0930b49099ea22737b
sakwook-real <sakwook@gmail.com>
2022-07-02 10:09:21 +0900

    Baekjoon Commit

---
+++ Baekjoon_Python_Shin/Baekjoon_7287_Shin.py
@@ -0,0 +1,9 @@
+# 백준 7287 등록
+# Baekjoon 7287
+
+# Created by sw0817 on 2022. 07. 02..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/7287
+
+print("480\nsakwook2")
