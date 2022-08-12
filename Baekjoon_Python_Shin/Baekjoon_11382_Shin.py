31606f08423be36cf3e6f15c1a30bfa51aff957f
sakwook-real <sakwook@gmail.com>
2022-08-12 22:14:13 +0900

    Baekjoon 0812 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_11382_Shin.py
@@ -0,0 +1,9 @@
+# 백준 11382 꼬마 정민
+# Baekjoon 11382
+
+# Created by sw0817 on 2022. 08. 12..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/11382
+
+print(sum(list(map(int, input().split()))))
