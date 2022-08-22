4ce9cee7803fee4798df04d10e163edd9a9a9cfb
sakwook-real <sakwook@gmail.com>
2022-08-22 22:43:08 +0900

    Baekjoon 0822 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_11718_Shin.py
@@ -0,0 +1,13 @@
+# 백준 11718 그대로 출력하기
+# Baekjoon 11718
+
+# Created by sw0817 on 2022. 08. 22..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/11718
+
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
---
+++ Baekjoon_Python_Shin/Baekjoon_9086_Shin.py
@@ -0,0 +1,11 @@
+# 백준 9086 문자열
+# Baekjoon 9086
+
+# Created by sw0817 on 2022. 08. 22..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/9086
+
+for _ in range(int(input())):
+    word = input()
+    print(word[0] + word[-1])
