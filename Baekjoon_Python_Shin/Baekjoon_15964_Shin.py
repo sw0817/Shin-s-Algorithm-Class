ec9375db5799af3ed274728c40febd542700d911
sakwook-real <sakwook@gmail.com>
2022-08-24 23:32:18 +0900

    Baekjoon 0824 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_15964_Shin.py
@@ -0,0 +1,10 @@
+# 백준 15964 이상한 기호
+# Baekjoon 15964
+
+# Created by sw0817 on 2022. 08. 24..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/15964
+
+a, b = map(int, input().split())
+print((a + b) * (a - b))
