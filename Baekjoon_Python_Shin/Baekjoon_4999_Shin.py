9d1dc173b20888a4df4abf56e83393d41cfa42f4
sakwook-real <sakwook@gmail.com>
2022-08-26 22:00:54 +0900

    Baekjoon 0826 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_4999_Shin.py
@@ -0,0 +1,9 @@
+# 백준 4999 아!
+# Baekjoon 4999
+
+# Created by sw0817 on 2022. 08. 26..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/4999
+
+print('no') if len(input()) < len(input()) else print('go')
