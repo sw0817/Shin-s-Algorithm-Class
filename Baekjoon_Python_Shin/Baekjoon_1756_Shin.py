d5b72222b4d8c7b5578c28ac75f0fc7d8354ca08
sakwook-real <sakwook@gmail.com>
2022-09-23 18:39:57 +0900

    Baekjoon 0923 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_1756_Shin.py
@@ -0,0 +1,34 @@
+# 백준 1756 피자 굽기
+# Baekjoon 1756
+
+# Created by sw0817 on 2022. 09. 23..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/1756
+
+D, N = map(int, input().split())
+ds = list(map(int, input().split()))
+pizza = list(map(int, input().split()))
+
+cur = ds[0]
+for i in range(1, D):
+    if ds[i] < cur:
+        cur = ds[i]
+    else:
+        ds[i] = cur
+
+c = 0
+for i in range(N):
+    p = pizza[i]
+    while ds and ds[-1] < p:
+        c += 1
+        ds.pop(-1)
+
+    if not ds:
+        print(0)
+        break
+
+    c += 1
+    ds.pop(-1)
+
+print(D - c + 1)
