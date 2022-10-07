eed1b56ba78da2c3a9877fc6555412949be5c988
sakwook-real <sakwook@gmail.com>
2022-10-07 21:51:46 +0900

    Baekjoon 1007 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_22846_Shin.py
@@ -0,0 +1,13 @@
+# 백준 22846 인증된 쉬운 게임
+# Baekjoon 22846
+
+# Created by sw0817 on 2022. 10. 07..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/22846
+
+N = int(input())
+if N == 2 or N == 6:
+    print('Kali')
+else:
+    print('Ringo')
