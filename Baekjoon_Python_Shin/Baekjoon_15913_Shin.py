905683d5d1f7597cd8f0a335adf24ed4728f41b3
sakwook-real <sakwook@gmail.com>
2022-10-14 21:46:44 +0900

    Baekjoon 1014 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_15913_Shin.py
@@ -0,0 +1,12 @@
+# 백준 15913 가위 바위 보 999
+# Baekjoon 15913
+
+# Created by sw0817 on 2022. 10. 14..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/15913
+
+import random
+
+for i in range(999):
+    print(random.randint(0, 2))
