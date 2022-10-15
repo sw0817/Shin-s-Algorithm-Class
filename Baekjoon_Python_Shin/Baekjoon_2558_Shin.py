2d047ecd0f61ed6d7b3011554462f39b331bb809
sakwook-real <sakwook@gmail.com>
2022-10-15 22:45:36 +0900

    Baekjoon 1015 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_2558_Shin.py
@@ -0,0 +1,10 @@
+# 백준 2558 A+B - 2
+# Baekjoon 2558
+
+# Created by sw0817 on 2022. 10. 15..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/2558
+
+A = int(input())
+print(A + int(input()))
