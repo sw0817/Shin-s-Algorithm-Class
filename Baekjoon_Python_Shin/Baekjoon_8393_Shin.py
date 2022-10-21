51539d989d6e1745490775e178cbf58bf04230cf
sakwook-real <sakwook@gmail.com>
2022-10-21 21:40:40 +0900

    Baekjoon 1021 #1

---
+++ Baekjoon_Python_Shin/Baekjoon_8393_Shin.py
@@ -0,0 +1,10 @@
+# 백준 8393 합
+# Baekjoon 8393
+
+# Created by sw0817 on 2022. 10. 21..
+# Copyright © 2022 sw0817. All rights reserved.
+
+# See : https://www.acmicpc.net/problem/8393
+
+n = int(input())
+print(n * (n+1) // 2)
